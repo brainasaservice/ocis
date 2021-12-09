531a8211dc27cc6399c30a9842454cdb75d436c0
saw-jan <saw.jan.grg3e@gmail.com>
2021-12-09 15:34:14 +0545

    build ocis from source

--- tests/parallelDeployAcceptance/drone/ocis/entrypoint-override.sh
+++ tests/parallelDeployAcceptance/drone/ocis/entrypoint-override.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
-ocis server &
+cd /drone/src/ocis/bin
+./ocis server &
 sleep 10
 
 # idp, glauth and accounts are not needed -> replaced by Keycloak and OpenLDAP
-ocis kill idp
-ocis kill glauth
-ocis kill accounts
+./ocis kill idp
+./ocis kill glauth
+./ocis kill accounts
 
 wait
